36895f8d3cc8b6ed5752df3576cbf9a043a0584f
bokhonin <bokhonin@gmail.com>
2017-09-11 00:40:44 +0300

    Initial commit

---
+++ app/src/main/java/ru/bokhonin/criminalintent/CrimeLab.java
@@ -0,0 +1,46 @@
+package ru.bokhonin.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+    private Context mAppContext;
+
+    private CrimeLab(Context appContext) {
+        mAppContext = appContext;
+        mCrimes = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
+    }
+
+    public static CrimeLab get(Context c) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+        }
+
+        return sCrimeLab;
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
